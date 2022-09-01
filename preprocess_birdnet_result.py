9a3b2fd28e3f69ce1ed97bc0af79b03131f182d1
Benjamin Cretois <benjamin.cretois@nina.no>
2022-09-01 16:28:40 +0200

    Add filename

--- preprocess_birdnet_result.py
+++ preprocess_birdnet_result.py
@@ -13,12 +13,17 @@ def add_prefix(item, filename):
     item["prefix"] = prefix
     return item
         
+def add_filename(item, filename):
+    file_name = filename.split('/')[-1].split('.')[0]
+    item["filename"] = file_name
+    return item      
+        
 def add_info(filename, parsed, prefix, index_location_folder):
     
     for item in parsed:
         if prefix:
             improved = add_prefix(item, filename)
+        improved = add_filename(item, filename)
         improved = add_location(item, filename, index_location_folder)
         yield item
-    
-
+        
