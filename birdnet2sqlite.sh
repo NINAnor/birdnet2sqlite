3b6dc44ab8ed6b8e9a94d538b5ce85d33d3c2625
Benjamin Cretois <benjamin.cretois@nina.no>
2022-09-05 12:04:15 +0200

    expose the out folder to docker

--- birdnet2sqlite.sh
+++ birdnet2sqlite.sh
@@ -4,12 +4,14 @@ set -euo pipefail
 
 BASE_FOLDER=$1
 DB_NAME=$2
-PREFIX=$3
+OUT_FOLDER=$(dirname $DB_NAME)
+IS_PREFIX=$3
 INDEX_LOCATION_FOLDER=$4
 
 docker run --rm \
     -v $BASE_FOLDER:$BASE_FOLDER \
+    -v $OUT_FOLDER:$OUT_FOLDER \
     --workdir $BASE_FOLDER \
     birdnet2sqlite \
     bash -c "find $BASE_FOLDER -type f -name '*.txt' | xargs \
-    /src/birdnet2sqlite.py --database_path $DB_NAME --prefix $PREFIX --index_location_folder $INDEX_LOCATION_FOLDER"
+    /src/birdnet2sqlite.py --database_path $DB_NAME --prefix $IS_PREFIX --index_location_folder $INDEX_LOCATION_FOLDER"
